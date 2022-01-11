5325a360d8e93d7055cb20cb66b63fe1a2abf307
soni1541 <81567177+soni1541@users.noreply.github.com>
2022-01-11 22:31:15 +0500

    level_5
    Была придумана реализация уровня через картинку и кнопки. Подготовлены картинки с животными

---
+++ level_5/level_5/level_5.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace level_5
+{
+    partial class level_5
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_rak = new System.Windows.Forms.Button();
+            this.btn_pauk = new System.Windows.Forms.Button();
+            this.btn_nasekomoe = new System.Windows.Forms.Button();
+            this.btn_mlekopit = new System.Windows.Forms.Button();
+            this.btn_riby = new System.Windows.Forms.Button();
+            this.btn_pemnovodik = new System.Windows.Forms.Button();
+            this.btn_presmik = new System.Windows.Forms.Button();
+            this.btn_ptica = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(68, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 223);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_rak
+            // 
+            this.btn_rak.Location = new System.Drawing.Point(491, 25);
+            this.btn_rak.Name = "btn_rak";
+            this.btn_rak.Size = new System.Drawing.Size(172, 33);
+            this.btn_rak.TabIndex = 1;
+            this.btn_rak.Text = "Ракообразные";
+            this.btn_rak.UseVisualStyleBackColor = true;
+            // 
+            // btn_pauk
+            // 
+            this.btn_pauk.Location = new System.Drawing.Point(491, 75);
+            this.btn_pauk.Name = "btn_pauk";
+            this.btn_pauk.Size = new System.Drawing.Size(172, 33);
+            this.btn_pauk.TabIndex = 2;
+            this.btn_pauk.Text = "Паукообразные";
+            this.btn_pauk.UseVisualStyleBackColor = true;
+            // 
+            // btn_nasekomoe
+            // 
+            this.btn_nasekomoe.Location = new System.Drawing.Point(491, 125);
+            this.btn_nasekomoe.Name = "btn_nasekomoe";
+            this.btn_nasekomoe.Size = new System.Drawing.Size(172, 33);
+            this.btn_nasekomoe.TabIndex = 3;
+            this.btn_nasekomoe.Text = "Насекомые";
+            this.btn_nasekomoe.UseVisualStyleBackColor = true;
+            // 
+            // btn_mlekopit
+            // 
+            this.btn_mlekopit.Location = new System.Drawing.Point(491, 174);
+            this.btn_mlekopit.Name = "btn_mlekopit";
+            this.btn_mlekopit.Size = new System.Drawing.Size(172, 33);
+            this.btn_mlekopit.TabIndex = 4;
+            this.btn_mlekopit.Text = "Млекопитающие";
+            this.btn_mlekopit.UseVisualStyleBackColor = true;
+            // 
+            // btn_riby
+            // 
+            this.btn_riby.Location = new System.Drawing.Point(491, 371);
+            this.btn_riby.Name = "btn_riby";
+            this.btn_riby.Size = new System.Drawing.Size(172, 33);
+            this.btn_riby.TabIndex = 8;
+            this.btn_riby.Text = "Рыбы";
+            this.btn_riby.UseVisualStyleBackColor = true;
+            // 
+            // btn_pemnovodik
+            // 
+            this.btn_pemnovodik.Location = new System.Drawing.Point(491, 322);
+            this.btn_pemnovodik.Name = "btn_pemnovodik";
+            this.btn_pemnovodik.Size = new System.Drawing.Size(172, 33);
+            this.btn_pemnovodik.TabIndex = 7;
+            this.btn_pemnovodik.Text = "Земноводные";
+            this.btn_pemnovodik.UseVisualStyleBackColor = true;
+            // 
+            // btn_presmik
+            // 
+            this.btn_presmik.Location = new System.Drawing.Point(491, 272);
+            this.btn_presmik.Name = "btn_presmik";
+            this.btn_presmik.Size = new System.Drawing.Size(172, 33);
+            this.btn_presmik.TabIndex = 6;
+            this.btn_presmik.Text = "Пресмыкающиеся";
+            this.btn_presmik.UseVisualStyleBackColor = true;
+            // 
+            // btn_ptica
+            // 
+            this.btn_ptica.Location = new System.Drawing.Point(491, 222);
+            this.btn_ptica.Name = "btn_ptica";
+            this.btn_ptica.Size = new System.Drawing.Size(172, 33);
+            this.btn_ptica.TabIndex = 5;
+            this.btn_ptica.Text = "Птицы";
+            this.btn_ptica.UseVisualStyleBackColor = true;
+            // 
+            // level_5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_riby);
+            this.Controls.Add(this.btn_pemnovodik);
+            this.Controls.Add(this.btn_presmik);
+            this.Controls.Add(this.btn_ptica);
+            this.Controls.Add(this.btn_mlekopit);
+            this.Controls.Add(this.btn_nasekomoe);
+            this.Controls.Add(this.btn_pauk);
+            this.Controls.Add(this.btn_rak);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "level_5";
+            this.Text = "Уровень 5";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button rak;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_nasekomoe;
+        private System.Windows.Forms.Button btn_mlekopit;
+        private System.Windows.Forms.Button btn_riby;
+        private System.Windows.Forms.Button btn_pemnovodik;
+        private System.Windows.Forms.Button btn_presmik;
+        private System.Windows.Forms.Button btn_ptica;
+        private System.Windows.Forms.Button btn_rak;
+        private System.Windows.Forms.Button btn_pauk;
+    }
+}
+
