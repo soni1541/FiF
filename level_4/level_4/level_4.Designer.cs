af46ed9c3ffdaecb7fe46a6229d27696ad9d0fb1
soni1541 <81567177+soni1541@users.noreply.github.com>
2022-01-11 22:24:05 +0500

    level_4
    Устранен неправильный порядок вывода на форму загадок. Добавлена подсказка от ФиФ

---
+++ level_4/level_4/level_4.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace level_4
+{
+    partial class level_4
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_check = new System.Windows.Forms.Button();
+            this.textBox_answer1 = new System.Windows.Forms.TextBox();
+            this.textBox_answer2 = new System.Windows.Forms.TextBox();
+            this.textBox_answer3 = new System.Windows.Forms.TextBox();
+            this.textBox_answer4 = new System.Windows.Forms.TextBox();
+            this.textBox_answer5 = new System.Windows.Forms.TextBox();
+            this.label_final = new System.Windows.Forms.Label();
+            this.pictureBox_belka = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_belka)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_check
+            // 
+            this.button_check.Location = new System.Drawing.Point(865, 100);
+            this.button_check.Name = "button_check";
+            this.button_check.Size = new System.Drawing.Size(141, 31);
+            this.button_check.TabIndex = 0;
+            this.button_check.Text = "Проверить";
+            this.button_check.UseVisualStyleBackColor = true;
+            this.button_check.Click += new System.EventHandler(this.button_check_Click);
+            // 
+            // textBox_answer1
+            // 
+            this.textBox_answer1.Location = new System.Drawing.Point(672, 50);
+            this.textBox_answer1.Name = "textBox_answer1";
+            this.textBox_answer1.Size = new System.Drawing.Size(141, 23);
+            this.textBox_answer1.TabIndex = 1;
+            // 
+            // textBox_answer2
+            // 
+            this.textBox_answer2.Location = new System.Drawing.Point(672, 79);
+            this.textBox_answer2.Name = "textBox_answer2";
+            this.textBox_answer2.Size = new System.Drawing.Size(141, 23);
+            this.textBox_answer2.TabIndex = 2;
+            // 
+            // textBox_answer3
+            // 
+            this.textBox_answer3.Location = new System.Drawing.Point(672, 108);
+            this.textBox_answer3.Name = "textBox_answer3";
+            this.textBox_answer3.Size = new System.Drawing.Size(141, 23);
+            this.textBox_answer3.TabIndex = 3;
+            // 
+            // textBox_answer4
+            // 
+            this.textBox_answer4.Location = new System.Drawing.Point(672, 137);
+            this.textBox_answer4.Name = "textBox_answer4";
+            this.textBox_answer4.Size = new System.Drawing.Size(141, 23);
+            this.textBox_answer4.TabIndex = 4;
+            // 
+            // textBox_answer5
+            // 
+            this.textBox_answer5.Location = new System.Drawing.Point(672, 166);
+            this.textBox_answer5.Name = "textBox_answer5";
+            this.textBox_answer5.Size = new System.Drawing.Size(141, 23);
+            this.textBox_answer5.TabIndex = 5;
+            // 
+            // label_final
+            // 
+            this.label_final.AutoSize = true;
+            this.label_final.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_final.Location = new System.Drawing.Point(275, 294);
+            this.label_final.Name = "label_final";
+            this.label_final.Size = new System.Drawing.Size(0, 20);
+            this.label_final.TabIndex = 6;
+            // 
+            // pictureBox_belka
+            // 
+            this.pictureBox_belka.Location = new System.Drawing.Point(779, 233);
+            this.pictureBox_belka.Name = "pictureBox_belka";
+            this.pictureBox_belka.Size = new System.Drawing.Size(280, 350);
+            this.pictureBox_belka.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_belka.TabIndex = 7;
+            this.pictureBox_belka.TabStop = false;
+            this.pictureBox_belka.Click += new System.EventHandler(this.pictureBox_belka_Click);
+            // 
+            // level_4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1094, 627);
+            this.Controls.Add(this.pictureBox_belka);
+            this.Controls.Add(this.label_final);
+            this.Controls.Add(this.textBox_answer5);
+            this.Controls.Add(this.textBox_answer4);
+            this.Controls.Add(this.textBox_answer3);
+            this.Controls.Add(this.textBox_answer2);
+            this.Controls.Add(this.textBox_answer1);
+            this.Controls.Add(this.button_check);
+            this.Name = "level_4";
+            this.Text = "Уровень 4";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_belka)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_check;
+        private System.Windows.Forms.TextBox textBox_answer1;
+        private System.Windows.Forms.TextBox textBox_answer2;
+        private System.Windows.Forms.TextBox textBox_answer3;
+        private System.Windows.Forms.TextBox textBox_answer4;
+        //private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox_answer5;
+        private System.Windows.Forms.Label label_final;
+        private System.Windows.Forms.PictureBox pictureBox_belka;
+    }
+}
+
---
+++ level_4/level_4/level_4.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace level_4
+{
+    public partial class level_4 : Form
+    {
+        private string path_to_file;
+        private int count_riddles;
+
+        private List<Riddle> riddles;
+        private List<Riddle> riddles_rand;
+        
+        public level_4()
+        {
+            InitializeComponent();
+
+            pictureBox_belka.Load("belka.png");
+            
+            path_to_file = "riddles.txt";
+            count_riddles = 5;
+
+            riddles = Riddle.read_riddles_from_file(path_to_file);
+
+            riddles_rand = Riddle.get_random_count_riddles(riddles, count_riddles);
+            
+            for (int idx = 0; idx < count_riddles; ++idx)
+            {
+                Label label_riddle = new Label();
+                label_riddle.Location = new Point(55, 35 + 30 * idx);
+                label_riddle.AutoSize = true;
+                label_riddle.Text = riddles_rand[idx].get_text();
+                Controls.Add(label_riddle);
+            }
+            
+        }
+
+        private void button_check_Click(object sender, EventArgs e)
+        {
+            
+            if (
+                (textBox_answer1.Text.ToLower() == riddles_rand[0].get_answer().ToLower()) && 
+
+                (textBox_answer2.Text.ToLower() == riddles_rand[1].get_answer().ToLower()) &&
+
+                (textBox_answer3.Text.ToLower() == riddles_rand[2].get_answer().ToLower()) &&
+
+                (textBox_answer4.Text.ToLower() == riddles_rand[3].get_answer().ToLower()) &&
+
+                (textBox_answer5.Text.ToLower() == riddles_rand[4].get_answer().ToLower())
+              )
+            {
+
+                label_final.Text = "Всё правильно! Молодец! Возьми с полки пирожек";
+            }
+            else
+            {
+                if(textBox_answer1.Text.Split(' ').Count() != 1 ||
+                textBox_answer2.Text.Split(' ').Count() != 1 ||
+                textBox_answer3.Text.Split(' ').Count() != 1 ||
+                textBox_answer4.Text.Split(' ').Count() != 1 ||
+                textBox_answer5.Text.Split(' ').Count() != 1)
+                {
+                    label_final.Text = "Ответ на загадку должен содержать только одно слово";
+                }
+
+                else if(textBox_answer1.Text.Length == 0 ||
+                        textBox_answer2.Text.Length == 0 ||
+                        textBox_answer3.Text.Length == 0 ||
+                        textBox_answer4.Text.Length == 0 ||
+                        textBox_answer5.Text.Length == 0)
+                {
+                    label_final.Text = "Ответь на все загадки. Попробуй еще раз";
+                }
+                else
+                {
+                    label_final.Text = "Попробуй ответить ещё раз";
+                }
+                    
+            }
+            
+        }
+
+        private void pictureBox_belka_Click(object sender, EventArgs e)
+        {
+            string help = "";
+            for (int idx = 0; idx < count_riddles; ++idx)
+            {
+                Random rnd = new Random();
+                string word = string.Join("", riddles_rand[idx].get_answer().ToLower().OrderBy(x => rnd.Next()));
+                help += word + "\n";
+            }
+                
+            label_final.Text = help;
+        }
+    }
+}
