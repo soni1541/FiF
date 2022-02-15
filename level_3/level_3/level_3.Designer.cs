eee075383b84d8febbea5ae97a9ffc9a8c38477b
soni1541 <81567177+soni1541@users.noreply.github.com>
2022-02-15 21:11:49 +0500

    level_3
    Генерация словарных слов с пропущенными буквами, проверка ответов

---
+++ level_3/level_3/level_3.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace level_3
+{
+    partial class level_3
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.word1 = new System.Windows.Forms.Label();
+            this.word2 = new System.Windows.Forms.Label();
+            this.word3 = new System.Windows.Forms.Label();
+            this.word4 = new System.Windows.Forms.Label();
+            this.word5 = new System.Windows.Forms.Label();
+            this.cbox_word1 = new System.Windows.Forms.ComboBox();
+            this.cbox_word2 = new System.Windows.Forms.ComboBox();
+            this.cbox_word3 = new System.Windows.Forms.ComboBox();
+            this.cbox_word4 = new System.Windows.Forms.ComboBox();
+            this.cbox_word5 = new System.Windows.Forms.ComboBox();
+            this.btn_result = new System.Windows.Forms.Button();
+            this.label_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // word1
+            // 
+            this.word1.AutoSize = true;
+            this.word1.Location = new System.Drawing.Point(40, 132);
+            this.word1.Name = "word1";
+            this.word1.Size = new System.Drawing.Size(50, 20);
+            this.word1.TabIndex = 0;
+            this.word1.Text = "label1";
+            // 
+            // word2
+            // 
+            this.word2.AutoSize = true;
+            this.word2.Location = new System.Drawing.Point(40, 182);
+            this.word2.Name = "word2";
+            this.word2.Size = new System.Drawing.Size(50, 20);
+            this.word2.TabIndex = 1;
+            this.word2.Text = "label2";
+            // 
+            // word3
+            // 
+            this.word3.AutoSize = true;
+            this.word3.Location = new System.Drawing.Point(40, 238);
+            this.word3.Name = "word3";
+            this.word3.Size = new System.Drawing.Size(50, 20);
+            this.word3.TabIndex = 2;
+            this.word3.Text = "label3";
+            // 
+            // word4
+            // 
+            this.word4.AutoSize = true;
+            this.word4.Location = new System.Drawing.Point(40, 299);
+            this.word4.Name = "word4";
+            this.word4.Size = new System.Drawing.Size(50, 20);
+            this.word4.TabIndex = 3;
+            this.word4.Text = "label4";
+            // 
+            // word5
+            // 
+            this.word5.AutoSize = true;
+            this.word5.Location = new System.Drawing.Point(40, 358);
+            this.word5.Name = "word5";
+            this.word5.Size = new System.Drawing.Size(50, 20);
+            this.word5.TabIndex = 4;
+            this.word5.Text = "label5";
+            // 
+            // cbox_word1
+            // 
+            this.cbox_word1.FormattingEnabled = true;
+            this.cbox_word1.Location = new System.Drawing.Point(213, 129);
+            this.cbox_word1.Name = "cbox_word1";
+            this.cbox_word1.Size = new System.Drawing.Size(151, 28);
+            this.cbox_word1.TabIndex = 5;
+            // 
+            // cbox_word2
+            // 
+            this.cbox_word2.FormattingEnabled = true;
+            this.cbox_word2.Location = new System.Drawing.Point(213, 182);
+            this.cbox_word2.Name = "cbox_word2";
+            this.cbox_word2.Size = new System.Drawing.Size(151, 28);
+            this.cbox_word2.TabIndex = 6;
+            // 
+            // cbox_word3
+            // 
+            this.cbox_word3.FormattingEnabled = true;
+            this.cbox_word3.Location = new System.Drawing.Point(213, 235);
+            this.cbox_word3.Name = "cbox_word3";
+            this.cbox_word3.Size = new System.Drawing.Size(151, 28);
+            this.cbox_word3.TabIndex = 7;
+            // 
+            // cbox_word4
+            // 
+            this.cbox_word4.FormattingEnabled = true;
+            this.cbox_word4.Location = new System.Drawing.Point(213, 291);
+            this.cbox_word4.Name = "cbox_word4";
+            this.cbox_word4.Size = new System.Drawing.Size(151, 28);
+            this.cbox_word4.TabIndex = 8;
+            // 
+            // cbox_word5
+            // 
+            this.cbox_word5.FormattingEnabled = true;
+            this.cbox_word5.Location = new System.Drawing.Point(213, 355);
+            this.cbox_word5.Name = "cbox_word5";
+            this.cbox_word5.Size = new System.Drawing.Size(151, 28);
+            this.cbox_word5.TabIndex = 9;
+            // 
+            // btn_result
+            // 
+            this.btn_result.Location = new System.Drawing.Point(213, 422);
+            this.btn_result.Name = "btn_result";
+            this.btn_result.Size = new System.Drawing.Size(151, 29);
+            this.btn_result.TabIndex = 10;
+            this.btn_result.Text = "Проверить";
+            this.btn_result.UseVisualStyleBackColor = true;
+            this.btn_result.Click += new System.EventHandler(this.btn_result_Click);
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Location = new System.Drawing.Point(40, 480);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(84, 20);
+            this.label_result.TabIndex = 11;
+            this.label_result.Text = "label_result";
+            // 
+            // level_3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.btn_result);
+            this.Controls.Add(this.cbox_word5);
+            this.Controls.Add(this.cbox_word4);
+            this.Controls.Add(this.cbox_word3);
+            this.Controls.Add(this.cbox_word2);
+            this.Controls.Add(this.cbox_word1);
+            this.Controls.Add(this.word5);
+            this.Controls.Add(this.word4);
+            this.Controls.Add(this.word3);
+            this.Controls.Add(this.word2);
+            this.Controls.Add(this.word1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "level_3";
+            this.Text = "Уровень 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label word1;
+        private System.Windows.Forms.Label word2;
+        private System.Windows.Forms.Label word3;
+        private System.Windows.Forms.Label word4;
+        private System.Windows.Forms.Label word5;
+        private System.Windows.Forms.ComboBox cbox_word1;
+        private System.Windows.Forms.ComboBox cbox_word2;
+        private System.Windows.Forms.ComboBox cbox_word3;
+        private System.Windows.Forms.ComboBox cbox_word4;
+        private System.Windows.Forms.ComboBox cbox_word5;
+        private System.Windows.Forms.Button btn_result;
+        private System.Windows.Forms.Label label_result;
+    }
+}
+
---
+++ level_3/level_3/level_3.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace level_3
+{
+    public partial class level_3 : Form
+    {
+        List<string> dictionary_for_level;
+        List<char> chars_upper;
+
+        public level_3()
+        {
+            InitializeComponent();
+
+            String path_to_file = "dictionary.txt";
+
+            string[] dictionary_from_file = File.ReadAllLines(path_to_file);
+
+            Random rnd = new Random();
+            int count_words_for_level = 5;
+            int random_key_word;
+
+            dictionary_for_level = new List<string>(count_words_for_level);
+
+            for (int idx = 0; idx < count_words_for_level; ++idx)
+            {
+                random_key_word = rnd.Next(0, dictionary_from_file.Count());
+                dictionary_for_level.Add(dictionary_from_file[random_key_word]);
+            }
+
+            chars_upper = new List<char>();
+
+            for (int i = 0; i < count_words_for_level; ++i)
+            {
+                for (int idx_l = 0; idx_l < dictionary_for_level[i].Length; ++idx_l)
+                {
+                    if (char.IsUpper(dictionary_for_level[i][idx_l]))
+                    {
+                        chars_upper.Add(dictionary_for_level[i][idx_l]);
+                        string word_has_not_letter = dictionary_for_level[i].Replace(dictionary_for_level[i][idx_l].ToString(), "_");
+                        dictionary_for_level[i] = word_has_not_letter;
+                    }
+                }
+            }
+
+            word1.Text = dictionary_for_level[0];
+            word2.Text = dictionary_for_level[1];
+            word3.Text = dictionary_for_level[2];
+            word4.Text = dictionary_for_level[3];
+            word5.Text = dictionary_for_level[4];
+
+            for (int i = 0; i < count_words_for_level; ++i)
+            {
+                cbox_word1.Items.Add(chars_upper[i]);
+                cbox_word2.Items.Add(chars_upper[i]);
+                cbox_word3.Items.Add(chars_upper[i]);
+                cbox_word4.Items.Add(chars_upper[i]);
+                cbox_word5.Items.Add(chars_upper[i]);
+            }
+
+            cbox_word1.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbox_word2.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbox_word3.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbox_word4.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbox_word5.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            cbox_word1.SelectedIndex = 0;
+            cbox_word2.SelectedIndex = 0;
+            cbox_word3.SelectedIndex = 0;
+            cbox_word4.SelectedIndex = 0;
+            cbox_word5.SelectedIndex = 0;
+        }
+
+        private void btn_result_Click(object sender, EventArgs e)
+        {
+            if(cbox_word1.SelectedIndex == 0 &&
+               cbox_word2.SelectedIndex == 1 &&
+               cbox_word3.SelectedIndex == 2 &&
+               cbox_word4.SelectedIndex == 3 &&
+               cbox_word5.SelectedIndex == 4)
+            {
+                label_result.Text = "Молодец";
+            }
+            else
+            {
+                label_result.Text = "Сверься со словарем и попробуй еще разок";
+            }
+        }
+    }
+}
